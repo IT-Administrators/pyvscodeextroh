1affc2860e95dee590c906730fac7a3b81b4e186
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2026-01-31 18:28:54 +0100

    Added README

--- script.py
+++
@@ -1,9 +0,0 @@
-from pyvscodeextroh import PolicyEditor
-
-editor = PolicyEditor()  # /etc/vscode/policy.json
-
-editor.allow_publisher("microsoft")
-editor.allow_publisher("github")
-editor.allow_extension("IT-Administrators.mdmanroh")
-editor.remove_publisher("IT-Administrators")
-editor.remove_extension("IT-Administrators.mdmanroh")
